62455e6260f4875af05cffe7008fdbcd4fa1b341
kritten <bugrpkvvuqrkcyorhobhnkiznizmvb>
2021-11-05 16:41:12 +0100

    +sections to steps, id and label to field

--- src/modules/creator/config.types.d.ts
+++ src/modules/creator/config.types.d.ts
@@ -1,11 +1,18 @@
 export type Field = {
   id: string;
+  label: string;
   type: 'select' | 'number' | 'text';
   data?: Array<string>;
+}
+
+export type Section = {
+  title?: string;
+  description?: string;
+  fields?: Array<Field>;
 }
 
 export type Step = {
   title: string;
   description: string;
-  fields?: Array<Field>;
+  sections?: Array<Section>;
 }
